897c46466dd18c4695fd13f79844883c1be24f32
Shirlei <schiufreitas@gmai.com>
2021-06-24 03:46:19 -0300

    setup projeto

---
+++ src/main/java/br/com/zupacademy/shirlei/casadocodigo/CasaDocodigoApplication.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.shirlei.casadocodigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasaDocodigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasaDocodigoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zupacademy/shirlei/casadocodigo/CasaDocodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.shirlei.casadocodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaDocodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
